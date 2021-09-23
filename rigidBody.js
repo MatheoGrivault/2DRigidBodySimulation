c3f95a1c97f0e6b98d7859dd32b2f8b0419060b2
Mathéo <matheo.grivault@gmail.com>
2021-09-23 23:41:08 +0200

    Update rigidBody.js

--- rigidBody.js
+++ rigidBody.js
@@ -3,6 +3,8 @@ class RigidBody{
     this.m = m
     this.p = p0
     this.v = v0
+    this.a = createVector(0, 0)
+    this.f = createVector(0, 0)
     this.lastUpdate = millis()
     this.radius = m/100
   }
@@ -11,17 +13,16 @@ update(){
     let t = (millis()-this.lastUpdate)/1000
     
     //Sum of forces
-    let netForce = p5.Vector.add(createVector(0, g.value()/100*this.m))
+    this.f = p5.Vector.add(createVector(0, g.value()/100*this.m))
     if(mouseIsPressed && mouseY < boxHeight){
       let attraction = createVector(mouseX-this.p.x, mouseY-this.p.y)
       attraction.setMag(attractionForce)
-      netForce.add(attraction)
+      this.f.add(attraction)
     }
     
     //a = F/m
-    let a = p5.Vector.div(netForce, this.m)
-      
-    this.v.add(p5.Vector.mult(a, t))
+    this.a = p5.Vector.div(this.f, this.m)
+    this.v.add(p5.Vector.mult(this.a, t))
     this.p.add(p5.Vector.mult(this.v, t))
     
     this.lastUpdate = millis()
@@ -30,5 +31,4 @@ update(){
   draw(){
     strokeWeight(this.radius*2)
     point(this.p.x, this.p.y)
-  }
-}
+    
